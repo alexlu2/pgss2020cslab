64f8d70119c48b36786c3305c256b6f8b342967b
alexlu2 <67604728+alexlu2@users.noreply.github.com>
2020-07-03 17:29:27 -0400

    Update myFirstRScript.R

--- MyNotebooks/myFirstRScript.R
+++ MyNotebooks/myFirstRScript.R
@@ -1,3 +1,25 @@
-a <- 1
+# Read Chicago COVID19 time series data into R as a data frame
+myData <- read.csv("https://raw.githubusercontent.com/menonpg/covid19_Chicago/master/timeSeries_COVID19_Chicago.csv")
 
-print(a)
+# Convert the date column appropriately so R recognizes it as a Date
+myData$Date <- as.Date(myData$Date, format="%Y-%m-%d")
+
+# Summarize the columns
+print(summary(myData)) 
+
+# Write the file out to the local file system:
+write.csv(myData, "timeSeries_COVID19_Chicago.csv")
+
+# Plot the data as a time series using XTS
+library(xts)   #install using # install.packages("xts")
+myDataXTS <- as.xts(myData[,-c(3)], order.by=myData$Date)
+plot(myDataXTS, main="Chicago cases and deaths relating to COVID19")
+
+# Repeat the plot using Plot_ly for an interactive plot
+library(plotly)
+p <- plot_ly(myData, x = ~Date, y = ~Cases, type = 'scatter', mode = 'lines') %>% 
+  add_trace( x = ~Date, y = ~Deaths, type = 'scatter', mode = 'lines') %>% 
+  layout(title = "Cases and Deaths in Chicago owing to COVID19",
+         xaxis = list(title = "Date"),
+         yaxis = list (title = "Cases / Deaths"))
+print(p)
